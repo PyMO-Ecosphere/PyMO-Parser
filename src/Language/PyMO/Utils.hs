a495073fa4ac02b2ca56fdfe11f64c74d9e3dc9e
许兴逸 <853974536@qq.com>
2024-02-28 17:51:15 +0800

    Utils

---
+++ src/Language/PyMO/Utils.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Language.PyMO.Utils
+  ( loadText
+  , loadTextLines
+  , commaCells
+  , strip) where
+
+import Prelude hiding ( lines )
+import Data.Text as T hiding ( lines, strip )
+import qualified Data.Text.IO.Utf8 as Utf8
+
+loadText :: FilePath -> IO Text
+loadText filePath = do
+  content <- Utf8.readFile filePath
+  if "\65279" `T.isPrefixOf` content
+    then return $ T.tail content
+    else return content
+
+lines :: Text -> [Text]
+lines text =
+  T.splitOn "\r\n" text
+  >>= T.splitOn "\r"
+  >>= T.splitOn "\n"
+
+loadTextLines :: FilePath -> IO [Text]
+loadTextLines = fmap lines . loadText
+
+commaCells :: T.Text -> [T.Text]
+commaCells x
+  | T.null x = []
+  | otherwise = T.splitOn "," x
+
+strip :: T.Text -> T.Text
+strip = T.dropWhile isSpace . T.dropWhileEnd isSpace
+  where
+    isSpace ' ' = True
+    isSpace '\t' = True
+    isSpace '\r' = True
+    isSpace _ = False
+
