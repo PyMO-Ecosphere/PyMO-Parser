9d9d98774c852024787cf9044da1075cacf4043e
许兴逸 <853974536@qq.com>
2024-02-29 03:48:35 +0800

    Bug fixed.

--- src/Language/PyMO/Utils.hs
+++ src/Language/PyMO/Utils.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE OverloadedStrings, PatternSynonyms #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE FlexibleInstances #-}
 
